591a75370a4799868e919b124268b86aa63f6282
esthercate <vuthi.esther@gmail.com>
2022-03-18 22:26:56 +0300

    adds jquery ui to collect user input

--- js/script.js
+++ js/script.js
@@ -14,8 +14,31 @@ OrderPizza.prototype.getTotal = function () {
 // User Interface Logic or Front-end
 
 $(document).ready(function () {
+  $("form#myForm1").submit(function (event) {
+    event.preventDefault();
+    var pizzaSize = $("input[name=size]:checked").val();
+    var pizzaCrust = $("input[name='crust']:checked").val();
+
+    var pizzaToppings = $('input:checkbox:checked').map(function () {
+      return this.value;
+    }).get();
+
+    var deliveryLocation = $("#deliveryLoc").val();
+
+    var res = pizzaSize + pizzaCrust + pizzaToppings + deliveryLocation;
+    console.log(res);
+
+  });
 
 
+  //Show input for delivery location
+  $(".option").click(function () {
+    $(".location").show();
+  });
+
+  $(".option2").click(function () {
+    $(".location").hide();
+  });
 
 
 });
