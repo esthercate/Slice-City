1ccf1605bd62dbb061fc6f6676b3ea70449a11b7
esthercate <vuthi.esther@gmail.com>
2022-03-19 21:30:15 +0300

    posts orders to a table

--- js/script.js
+++ js/script.js
@@ -1,7 +1,8 @@
 //Business Logic or Back-end
 
-function OrderPizza(size, crust, toppings, deliveryCost) {
-  this.pSize = size;
+function OrderPizza(name, size, crust, toppings, deliveryCost) {
+  this.pName = name,
+    this.pSize = size;
   this.pCrust = crust;
   this.pToppings = toppings;
   this.deliveryCost = deliveryCost;
@@ -17,6 +18,7 @@ OrderPizza.prototype.getTotal = function () {
 $(document).ready(function () {
   $("form#myForm1").submit(function (event) {
     event.preventDefault();
+    var pizzaName = $("input[name=pname]:checked").val();
     var pizzaSize = parseInt($("input[name=size]:checked").val());
     var pizzaCrust = parseInt($("input[name='crust']:checked").val());
     var arr = $('input:checkbox:checked').map(function () { // getting all checked inputs 
@@ -28,20 +30,20 @@ $(document).ready(function () {
     }, 0);
 
     var deliveryFee = parseInt($("input[name=delivery]:checked").val());
-    var deliveryLocation = $("#deliveryLoc").val()
+    var deliveryLocation = $("#deliveryLoc").val();
 
+    var total = pizzaSize + pizzaCrust + pizzaToppings + deliveryFee;
+    var newPizza = new OrderPizza(pizzaSize, pizzaCrust, pizzaToppings, deliveryFee);
+    $("#summary tbody").append("<tr>" + "<td> Pizza Name </td>" + "<td>" + pizzaName + "</td>" + "</tr>");
+    $("#summary tbody").append("<tr>" + "<td> Size Price </td>" + "<td>" + pizzaSize + "</td>" + "</tr>");
+    $("#summary tbody").append("<tr>" + "<td> Crust Price </td>" + "<td>" + pizzaCrust + "</td>" + "</tr>");
+    $("#summary tbody").append("<tr>" + "<td> Toppings Price </td>" + "<td>" + pizzaToppings + "</td>" + "</tr>");
+    $("#summary tbody").append("<tr>" + "<td> Delivery Fee </td>" + "<td>" + deliveryFee + "</td>" + "</tr>");
+    $("#summary tbody").append("<tr>" + "<td> Delivery Location </td>" + "<td>" + deliveryLocation + "</td>" + "</tr>");
+    $("#summary tbody").append("<tr>" + "<td> TOTAL PRICE </td>" + "<td>" + "Ksh. " + total + "</td>" + "</tr>");
 
-    var res = pizzaSize + pizzaCrust + pizzaToppings + deliveryFee;
-
-    $(".demo").append(res);
-
-
-    //console.log(res);
-    //console.log(deliveryLocation);
-
-
+    $("#myForm1").trigger("reset");
   });
-
 
   //Show input for delivery location
   $(".option").click(function () {
