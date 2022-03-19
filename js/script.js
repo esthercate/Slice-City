7adc12d4cd383b7efd4fd9bf116a987005e56cba
esthercate <vuthi.esther@gmail.com>
2022-03-19 14:36:41 +0300

    adds code to calculate total price

--- js/script.js
+++ js/script.js
@@ -1,15 +1,15 @@
 //Business Logic or Back-end
 
-function OrderPizza(size, crust, toppings) {
+function OrderPizza(size, crust, toppings, deliveryCost) {
   this.pSize = size;
   this.pCrust = crust;
   this.pToppings = toppings;
+  this.deliveryCost = deliveryCost;
 }
 
 OrderPizza.prototype.getTotal = function () {
-  return this.size + this.crust + this.toppings;
+  return this.size + this.crust + this.toppings + this.deliveryCost;
 };
-
 
 
 // User Interface Logic or Front-end
@@ -19,9 +19,6 @@ $(document).ready(function () {
     event.preventDefault();
     var pizzaSize = parseInt($("input[name=size]:checked").val());
     var pizzaCrust = parseInt($("input[name='crust']:checked").val());
-    var deliveryFee = parseInt($("input[name=delivery]:checked").val());
-    var deliveryLocation = $("#deliveryLoc").val()
-
     var arr = $('input:checkbox:checked').map(function () { // getting all checked inputs 
       return parseInt(this.value); // converting the input to numbers 
     }).get();
@@ -30,10 +27,19 @@ $(document).ready(function () {
       return a + b;
     }, 0);
 
+    var deliveryFee = parseInt($("input[name=delivery]:checked").val());
+    var deliveryLocation = $("#deliveryLoc").val()
 
-    //var res = pizzaSize + pizzaCrust + pizzaToppings + deliveryFee;
+
+    var res = pizzaSize + pizzaCrust + pizzaToppings + deliveryFee;
+
+    $(".demo").append(res);
+
+
     //console.log(res);
     //console.log(deliveryLocation);
+
+
   });
 
 
